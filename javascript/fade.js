f9664b09abfe2dd145f6d940ded5680890fa428a
yiwei <wangyiwei11259@gmail.com>
2018-03-13 03:23:56 +0800

    Add an inner div of each member card (in order to slim the card) and modify three layouts.

--- javascript/fade.js
+++ javascript/fade.js
@@ -14,7 +14,7 @@ var member_count = 0;
 
 var c2a_div = document.getElementById("c2a");
 var c2a_item = document.getElementsByClassName("c2a-item"); //c2a_item[0] is introduction.
-var c2a_item_right = document.getElementsByClassName("c2a-item-right")[0];
+var c2a_item_center = document.getElementsByClassName("c2a-item")[1];
 var c2a_logo = document.getElementById("action-logo");
 var c2a_btn = document.getElementById("herobtn2");
 var c2a_intro_offset = c2a_div.offsetTop - hero_div.offsetHeight;
@@ -57,8 +57,8 @@ window.onscroll = function () {
             c2a_item[i].style.opacity = 1;
         }
         c2a_count++;
-        c2a_item_right.style.transitionDelay = c2a_delay + "s";
-        c2a_item_right.style.opacity = 1;
+        c2a_item_center.style.transitionDelay = c2a_delay + "s";
+        c2a_item_center.style.opacity = 1;
         c2a_btn.style.transform = "scale(1.2)";
         var myVar = setTimeout(shrink, 800);
         function shrink() {
