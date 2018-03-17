d2235d71e34dd3d7938b7688ff42bd07e8789e7a
yiwei <wangyiwei11259@gmail.com>
2018-03-17 21:54:55 +0800

    Add our-work and partners sections fade animation

--- javascript/fade.js
+++ javascript/fade.js
@@ -5,14 +5,27 @@ var introductions = document.getElementsByClassName("introduction");
 var features_div = document.getElementById("features");
 var feature_item = document.getElementsByClassName("feature-item");
 var feature_intro_offset = features_div.offsetTop - hero_div.offsetHeight;
-var feature_item_offset = (features_div.offsetTop - hero_div.offsetHeight) + introductions[0].offsetHeight;
+var feature_item_offset = (feature_intro_offset) + introductions[0].offsetHeight;
 var feature_count = 0;
+
+var works_div = document.getElementById('works');
+var works_item = document.getElementsByClassName("works-item");
+var works_intro_offset = works_div.offsetTop - hero_div.offsetHeight;
+var works_item_offset = works_intro_offset + introductions[1].offsetHeight;
+var works_count = 0;
+
 
 var members_div = document.getElementById("members");
 var member_item = document.getElementsByClassName("member-item"); 
 var member_intro_offset = members_div.offsetTop - hero_div.offsetHeight;
-var member_item_offset = (members_div.offsetTop - hero_div.offsetHeight) + introductions[1].offsetHeight;
+var member_item_offset = (member_intro_offset) + introductions[2].offsetHeight;
 var member_count = 0;
+
+var partners_div = document.getElementById('partners');
+var partners_item = document.getElementsByClassName("partners-item");
+var partners_intro_offset = partners_div.offsetTop - hero_div.offsetHeight;
+var partners_item_offset = partners_intro_offset + introductions[3].offsetHeight;
+var partners_count = 0;
 
 var c2a_div = document.getElementById("c2a");
 var c2a_item = document.getElementsByClassName("c2a-item");
@@ -20,7 +33,7 @@ var c2a_item_center = document.getElementsByClassName("c2a-item")[1];
 var c2a_logo = document.getElementById("action-logo");
 var c2a_btn = document.getElementById("herobtn2");
 var c2a_intro_offset = c2a_div.offsetTop - hero_div.offsetHeight;
-var c2a_item_offset = (c2a_div.offsetTop - hero_div.offsetHeight) + introductions[2].offsetHeight;
+var c2a_item_offset = (c2a_intro_offset) + introductions[4].offsetHeight;
 
 var c2a_count = 0;
 
@@ -36,8 +49,20 @@ window.onscroll = function () {
         }
         feature_count++;
     }
-    if (member_count == 0 && (window.scrollY > member_intro_offset)) {
+    if (works_count == 0 && (window.scrollY > works_intro_offset)) {
         introductions[1].style.opacity = 1;
+        works_count++;
+    }
+    if (works_count == 1 && (window.scrollY > works_item_offset)) {
+        var works_delay = 0;
+        for (var i = 0; i < works_item.length; ++i, works_delay += 0.3) {
+            works_item[i].style.transitionDelay = works_delay + "s";
+            works_item[i].style.opacity = 1;
+        }
+        works_count++;
+    }
+    if (member_count == 0 && (window.scrollY > member_intro_offset)) {
+        introductions[2].style.opacity = 1;
         member_count++;
     }
     if (member_count == 1 && (window.scrollY > member_item_offset)) {
@@ -48,11 +73,23 @@ window.onscroll = function () {
         }
         member_count++;
     }
-    // if (c2a_count == 0 && (window.scrollY > c2a_intro_offset)) {
-    //     introductions[2].style.opacity = 1;
-    //     c2a_count++;
-    // }
-    if (c2a_count == 0 && (window.scrollY > c2a_item_offset)) {
+    if (partners_count == 0 && (window.scrollY > partners_intro_offset)) {
+        introductions[3].style.opacity = 1;
+        partners_count++;
+    }
+    if (partners_count == 1 && (window.scrollY > partners_item_offset)) {
+        var partners_delay = 0;
+        for (var i = 0; i < partners_item.length; ++i, partners_delay += 0.3) {
+            partners_item[i].style.transitionDelay = partners_delay + "s";
+            partners_item[i].style.opacity = 1;
+        }
+        partners_count++;
+    }
+    if (c2a_count == 0 && (window.scrollY > c2a_intro_offset)) {
+        introductions[2].style.opacity = 1;
+        c2a_count++;
+    }
+    if (c2a_count == 1 && (window.scrollY > c2a_item_offset)) {
         var c2a_delay = 0;
         for (var i = 0; i < c2a_item.length; ++i, c2a_delay += 0.3) {
             c2a_item[i].style.transitionDelay = c2a_delay + "s";
