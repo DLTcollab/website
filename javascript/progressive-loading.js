d7d5db779e523967955b3c3b8e099aa33911df28
yiwei <wangyiwei11259@gmail.com>
2018-04-27 18:42:31 +0800

    Optimize hero image

--- javascript/progressive-loading.js
+++ javascript/progressive-loading.js
@@ -1,1 +1,12 @@
-var heroDiv = document.getElementById('hero');
+var hero_container = document.querySelector('.hero-container');
+
+document.addEventListener("DOMContentLoaded", function() {
+  if (!hero_container) return !1;
+  var origin_src = hero_container.getAttribute("data-src"),
+      img = new Image;
+
+  img.src = origin_src;
+  img.onload = function () {
+    hero_container.style.backgroundImage = 'url(' + origin_src + ')';
+  };
+});
